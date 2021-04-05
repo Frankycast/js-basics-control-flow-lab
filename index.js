78c1ac4a32f072291a93b117fe4df6e23c5898f6
Francisco Castillo <Francisco21castillo@aol.com>
2021-04-05 16:25:14 -0400

    commit tree 003ad2095c99e46b3d49c2ec0262af48dc733488

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400){
+  return "This one is on me!";
+  } else if (someValue > 2500) {
+    return 'No can do.';
+  } else if (someValue > 2000){
+    return 'I will gladly take your thirty bucks.';
+  }
+  
 }
+scuberGreetingForFeet(199)
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  
+function ternaryCheckCity(city){
+  if (city === `NYC`) {
+    return `Ok, sounds good.`;
+  } else {
+    return`No go.`
+  }
+  }
+// Write your code here!
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+
+  if ( tip === `generous`){
+    return `Thank you so much.`;
+  }else if (tip === `not as generous`){
+    return `Thank you.`;
+  }else{
+    return`Bye.`;
+  }
 }
